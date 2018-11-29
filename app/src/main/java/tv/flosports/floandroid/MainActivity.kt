9bfc812abe9684fe6f5a61d5ac6269c1ee5394f4
Peter Graham <pwgraham91@gmail.com>
2018-11-29 15:14:17 -0600

    basic http call

--- app/src/main/java/tv/flosports/floandroid/MainActivity.kt
+++ app/src/main/java/tv/flosports/floandroid/MainActivity.kt
@@ -4,13 +4,57 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+data class ViewModel(var message: String = "original message", var number: Int = 0)
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel = ViewModel()
+
+    private fun applyViewModel() {
+        println("${viewModel.message} ${viewModel.number}")
+        this.runOnUiThread {
+            message.text = viewModel.message
+        }
+    }
+
+    fun getBasicHttp() {
+        val url = "https://httpbin.org/get"
+        val obj = URL(url)
+
+        with(obj.openConnection() as HttpURLConnection) {
+            // optional default is GET
+            requestMethod = "GET"
+
+
+            println("\nSending 'GET' request to URL : $url")
+            println("Response Code : $responseCode")
+
+            BufferedReader(InputStreamReader(inputStream)).use {
+                val response = StringBuffer()
+
+                var inputLine = it.readLine()
+                while (inputLine != null) {
+                    response.append(inputLine)
+                    inputLine = it.readLine()
+                }
+                println(response.toString())
+                viewModel.message = "the new message"
+                applyViewModel()
+            }
+        }
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                doAsync {
+                    getBasicHttp()
+                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
@@ -30,5 +74,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        doAsync {
+            viewModel.message = "on create message"
+            applyViewModel()
+            getBasicHttp()
+        }
     }
 }
